327b5bc780cad64f19894e29d482618e7d6b97b8
caseyyee <caseyyee.ca@gmail.com>
2017-11-09 17:58:53 -0800

    cleans up camera script

--- Assets/Scripts/WebVRCamera.cs
+++ Assets/Scripts/WebVRCamera.cs
@@ -21,24 +21,30 @@ public class WebVRCamera : MonoBehaviour
     Quaternion rhq;
     Vector3 cp;
 
+	// left and right hand position and rotation
 	Vector3 lhp;
 	Vector3 rhp;
 	Quaternion lhr;
 	Quaternion rhr;
+
+	// camera view and projection matrices
 	Matrix4x4 clp = Matrix4x4.identity;
 	Matrix4x4 clv = Matrix4x4.identity;
 	Matrix4x4 crp = Matrix4x4.identity;
 	Matrix4x4 crv = Matrix4x4.identity;
-	Matrix4x4 sitStand = Matrix4x4.identity;
-	float deltaTime = 0.0f;
 
+	// sit stand room transform
+	Matrix4x4 sitStand = Matrix4x4.Translate (new Vector3 (0, 1.2f, 0));
 
-    bool active = false;
-    private Vector3 rotation;
-
-    public GameObject leftHandObj;
+    bool active = false; // vr mode
+    
+	public GameObject leftHandObj;
     public GameObject rightHandObj;
+
 	public bool handControllers = false;
+
+	// delta time for latency checker.
+	float deltaTime = 0.0f;
 
 	[System.Serializable]
 	public class Gamepads
@@ -63,9 +69,172 @@ public class WebVRCamera : MonoBehaviour
 		{
 			return JsonUtility.FromJson<Controller>(jsonString);
 		}
-
 	}
 
+	// received enter VR from WebVR browser
+	public void Begin()
+	{
+		changeMode("vr");
+	}
+
+	// receive exit VR from WebVR browser
+	public void End()
+	{
+		changeMode("normal");
+	}
+
+	// receive view and projection matrices from WebVR browser
+	public void HMDViewProjection (string viewProjectionNumbersStr) {
+		float[] array = viewProjectionNumbersStr.Split(',').Select(float.Parse).ToArray();
+		// left projection matrix
+		clp = numbersToMatrix(array.Skip(16 * 0).Take (16).ToArray ());
+		// left view matrix
+		clv = numbersToMatrix(array.Skip(16 * 1).Take (16).ToArray ());
+		// right projection matrix
+		crp = numbersToMatrix(array.Skip(16 * 2).Take (16).ToArray ());
+		// right view matrix
+		crv = numbersToMatrix(array.Skip(16 * 3).Take (16).ToArray ());
+	}
+
+	// received sit and stand room transform from WebVR browser
+	public void HMDSittingToStandingTransform (string sitStandStr) {
+		float[] array = sitStandStr.Split(',').Select(float.Parse).ToArray();
+		sitStand = numbersToMatrix (array);
+	}
+
+	// receive gamepad data from WebVR browser
+	public void VRGamepads (string jsonString) {
+		Gamepads list = Gamepads.CreateFromJSON(jsonString);
+
+		handControllers = list.controllers.Length > 0 ? true : false;
+
+		foreach (Controller control in list.controllers) {
+			float[] pos = control.position.Split(',').Select(float.Parse).ToArray();
+			float[] rot = control.orientation.Split(',').Select(float.Parse).ToArray();
+			Vector3 position = new Vector3 (pos [0], pos [1], pos [2]);
+			Quaternion rotation = new Quaternion (rot [0], rot [1], rot [2], rot[3]);
+
+			Quaternion sitStandRotation = Quaternion.LookRotation (
+				sitStand.GetColumn (2),
+				sitStand.GetColumn (1)
+			);
+			Vector3 p = sitStand.MultiplyPoint(position);
+			Quaternion r = rotation * sitStandRotation;
+
+			if (control.hand == "left") {
+				lhp = p;
+				lhr = r;
+			}
+			if (control.hand == "right") {
+				rhp = p;
+				rhr = r;
+			}
+		}			
+	}
+
+	// received time tester from WebVR browser
+	public void TestTime() {
+		Debug.Log ("Time tester received in Unity");
+		TestTimeReturn ();
+	}
+
+	private void toggleMode() {
+		active = active == true ? false : true;
+		string mode = active == true ? "vr" : "normal";
+		changeMode (mode);
+	}
+
+	private void changeMode(string mode)
+	{
+		Debug.Log("Switching to " + mode);
+
+		switch (mode)
+		{
+		case "normal":
+			cameraMain.enabled = true;
+			cameraL.enabled = false;
+			cameraR.enabled = false;
+			active = false;
+			break;
+		case "vr":
+			cameraMain.enabled = false;
+			cameraL.enabled = true;
+			cameraR.enabled = true;
+			active = true;
+			break;
+		}
+	}
+
+	private IEnumerator endOfFrame()
+	{
+		// wait until end of frame to report back to WebVR browser to submit frame.
+		yield return new WaitForEndOfFrame();
+		PostRender ();
+	}
+
+	void Start()
+	{
+		cameraMain = GameObject.Find("CameraMain").GetComponent<Camera>();
+		cameraL = GameObject.Find("CameraL").GetComponent<Camera>();
+		cameraR = GameObject.Find("CameraR").GetComponent<Camera>();
+
+		changeMode("normal");
+
+		#if !UNITY_EDITOR && UNITY_WEBGL
+		FinishLoading();
+		#endif
+	}
+
+	void Update()
+	{
+		deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+
+		if (Input.GetKeyDown("space")) 
+		{
+			toggleMode ();
+		}
+
+		if (active == true) {
+			leftHandObj.transform.rotation = lhr;
+			leftHandObj.transform.position = lhp;
+			rightHandObj.transform.rotation = rhr;
+			rightHandObj.transform.position = rhp;
+
+			// apply camera projection and view matrices from webVR api.
+			if (!clv.isIdentity || !clp.isIdentity || !crv.isIdentity || !crp.isIdentity) {
+				// apply sit stand transform
+				clv *= sitStand.inverse;
+				crv *= sitStand.inverse;
+
+				cameraL.worldToCameraMatrix = clv;
+				cameraL.projectionMatrix = clp;
+				cameraR.worldToCameraMatrix = crv;
+				cameraR.projectionMatrix = crp;
+			}
+		}
+
+		#if !UNITY_EDITOR && UNITY_WEBGL
+		StartCoroutine(endOfFrame());
+		#endif
+	}
+
+	void OnGUI()
+	{
+		int w = Screen.width, h = Screen.height;
+
+		GUIStyle style = new GUIStyle();
+
+		Rect rect = new Rect(w / 4, h / 2, w, h * 2 / 100);
+		style.alignment = TextAnchor.UpperLeft;
+		style.fontSize = h * 2 / 100;
+		style.normal.textColor = new Color (0.0f, 1.0f, 1.0f, 1.0f);
+		float msec = deltaTime * 1000.0f;
+		float fps = 1.0f / deltaTime;
+		string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+		GUI.Label(rect, text, style);
+	}
+		
+	// Utility functions
 	private Matrix4x4 numbersToMatrix(float[] array) {
 		var mat = new Matrix4x4 ();
 		mat.m00 = array[0];
@@ -85,178 +254,5 @@ public class WebVRCamera : MonoBehaviour
 		mat.m32 = array[14];
 		mat.m33 = array[15];
 		return mat;
-	}
-		
-
-	// time tester
-	void TestTime() {
-		Debug.Log ("Time tester received in Unity");
-		TestTimeReturn ();
-	}
-		
-
-	// hmd start frame.
-	public void startFrame() {
-	}
-
-	// Send post render update so we can submitFrame to vrDisplay.
-	private IEnumerator endOfFrame()
-	{
-		yield return new WaitForEndOfFrame();
-		PostRender ();
-	}
-
-	public void HMDViewProjection (string viewProjectionNumbersStr) {
-		float[] array = viewProjectionNumbersStr.Split(',').Select(float.Parse).ToArray();
-		// left projection matrix
-		clp = numbersToMatrix(array.Skip(16 * 0).Take (16).ToArray ());
-		// left view matrix
-		clv = numbersToMatrix(array.Skip(16 * 1).Take (16).ToArray ());
-		// right projection matrix
-		crp = numbersToMatrix(array.Skip(16 * 2).Take (16).ToArray ());
-		// right view matrix
-		crv = numbersToMatrix(array.Skip(16 * 3).Take (16).ToArray ());
-	}
-
-	public void VRGamepads (string jsonString) {
-		Gamepads list = Gamepads.CreateFromJSON(jsonString);
-
-		handControllers = list.controllers.Length > 0 ? true : false;
-
-		foreach (Controller control in list.controllers) {
-			float[] pos = control.position.Split(',').Select(float.Parse).ToArray();
-			float[] rot = control.orientation.Split(',').Select(float.Parse).ToArray();
-			Vector3 position = new Vector3 (pos [0], pos [1], pos [2]);
-			Quaternion rotation = new Quaternion (rot [0], rot [1], rot [2], rot[3]);
-
-			Quaternion sitStandRotation = Quaternion.LookRotation (
-				                              sitStand.GetColumn (2),
-				                              sitStand.GetColumn (1)
-			                              );
-			Vector3 p = sitStand.MultiplyPoint(position);
-			Quaternion r = rotation * sitStandRotation;
-
-			if (control.hand == "left") {
-				lhp = p;
-				lhr = r;
-			}
-			if (control.hand == "right") {
-				rhp = p;
-				rhr = r;
-			}
-		}			
-	}
-
-	public void HMDSittingToStandingTransform (string sitStandStr) {
-		float[] array = sitStandStr.Split(',').Select(float.Parse).ToArray();
-		sitStand = numbersToMatrix (array);
-	}
-
-    public void Begin()
-    {
-		changeMode("vr");
-    }
-
-	public void End()
-	{
-		changeMode("normal");
-	}
-
-	void toggleMode() {
-		active = active == true ? false : true;
-		string mode = active == true ? "vr" : "normal";
-		changeMode (mode);
-	}
-
-	void changeMode(string mode)
-	{
-		Debug.Log("Switching to " + mode);
-
-		switch (mode)
-		{
-		case "normal":
-			cameraMain.GetComponent<Camera> ().enabled = true;
-			cameraL.GetComponent<Camera> ().enabled = false;
-			cameraR.GetComponent<Camera> ().enabled = false;
-			active = false;
-			break;
-		case "vr":
-			cameraMain.GetComponent<Camera>().enabled = false;
-			cameraL.GetComponent<Camera>().enabled = true;
-			cameraR.GetComponent<Camera>().enabled = true;
-			active = true;
-			break;
-		}
-	}
-		
-    void Start()
-    {
-		cameraMain = GameObject.Find("CameraMain").GetComponent<Camera>();
-		cameraL = GameObject.Find("CameraL").GetComponent<Camera>();
-		cameraR = GameObject.Find("CameraR").GetComponent<Camera>();
-
-		changeMode("normal");
-
-		// default sitStand translation.
-		sitStand = Matrix4x4.Translate (new Vector3 (0f, 1.2f, 0f));
-
-		#if !UNITY_EDITOR && UNITY_WEBGL
-		FinishLoading();
-		#endif
-    }
-
-
-
-    void Update()
-    {
-		deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
-
-		if (Input.GetKeyDown("space")) 
-		{
-			toggleMode ();
-		}
-
-
-        if (active == true)
-        {
-			leftHandObj.transform.rotation = lhr;
-			leftHandObj.transform.position = lhp;
-            rightHandObj.transform.rotation = rhr;
-			rightHandObj.transform.position = rhp;
-
-			// apply sit stand transform
-			clv *= sitStand.inverse;
-			crv *= sitStand.inverse;
-
-			// apply camera projection and view matrices from webVR api.
-			if (!clv.isIdentity || !clp.isIdentity || !crv.isIdentity || !crp.isIdentity) {
-				cameraL.worldToCameraMatrix = clv;
-				cameraL.projectionMatrix = clp;
-				cameraR.worldToCameraMatrix = crv;
-				cameraR.projectionMatrix = crp;
-			}
-        }
-
-		#if !UNITY_EDITOR && UNITY_WEBGL
-		StartCoroutine(endOfFrame());
-		#endif
-    }
-
-
-
-	void OnGUI()
-	{
-		int w = Screen.width, h = Screen.height;
-
-		GUIStyle style = new GUIStyle();
-
-		Rect rect = new Rect(w / 4, h / 2, w, h * 2 / 100);
-		style.alignment = TextAnchor.UpperLeft;
-		style.fontSize = h * 2 / 100;
-		style.normal.textColor = new Color (0.0f, 1.0f, 1.0f, 1.0f);
-		float msec = deltaTime * 1000.0f;
-		float fps = 1.0f / deltaTime;
-		string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
-		GUI.Label(rect, text, style);
 	}
 }
