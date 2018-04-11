01be266160a016c4ccc14a6900c8f952bb3289c3
caseyyee <caseyyee.ca@gmail.com>
2018-04-11 16:35:57 -0700

    Smoother FreeFlightController

--- Assets/WebVR/Scripts/Input/FreeFlightController.cs
+++ Assets/WebVR/Scripts/Input/FreeFlightController.cs
@@ -4,195 +4,114 @@ using UnityEngine.EventSystems;
 using UnityEngine;
 
 public class FreeFlightController : MonoBehaviour {
-	[Tooltip("Enable/disable rotation control. For use in Unity editor only.")]
-	public bool rotationEnabled = true;
+    [Tooltip("Enable/disable rotation control. For use in Unity editor only.")]
+    public bool rotationEnabled = true;
 
-	[Tooltip("Enable/disable translation control. For use in Unity editor only.")]
-	public bool translationEnabled = true;
+    [Tooltip("Enable/disable translation control. For use in Unity editor only.")]
+    public bool translationEnabled = true;
 
-	[Tooltip("Speed of rotation in degrees/seconds.")]
-	public float rotationSpeed = 90;
+    private WebVRManager webVRManager;
+    private WebVRDisplayCapabilities capabilities;
 
-	[Tooltip("Speed of translation in meters/seconds.")]
-	public float translationSpeed = 7;
+    [Tooltip("Mouse sensitivity")]
+    public float mouseSensitivity = 1f;
 
-	[Tooltip("Minimum distance the mouse must move to start registering movement.")]
-	public float rotationDeadDistance = 0.001f;
+    [Tooltip("Straffe Speed")]
+    public float straffeSpeed = 5f;
 
-	[Tooltip("Keys to move forward.")]
-	public List<KeyCode> moveForwardKeys =
-		new List<KeyCode> { KeyCode.W, KeyCode.UpArrow };
+    private float minimumX = -360f;
+    private float maximumX = 360f;
 
-	[Tooltip("Keys to move backward.")]
-	public List<KeyCode> moveBackwardKeys =
-		new List<KeyCode> { KeyCode.S, KeyCode.DownArrow };
+    private float minimumY = -90f;
+    private float maximumY = 90f;
 
-	[Tooltip("Keys to stride to the right.")]
-	public List<KeyCode> strideRightKeys =
-		new List<KeyCode> { KeyCode.D, KeyCode.RightArrow };
+    private float rotationX = 0f;
+    private float rotationY = 0f;
 
-	[Tooltip("Keys to stride to the left.")]
-	public List<KeyCode> strideLeftKeys =
-		new List<KeyCode> { KeyCode.A, KeyCode.LeftArrow };
+    Quaternion originalRotation;
 
-	[Tooltip("Keys to move upward.")]
-	public List<KeyCode> moveUpwardKeys = new List<KeyCode> { KeyCode.R };
+    bool inDesktopLike {
+        get {
+            return capabilities.hasExternalDisplay;
+        }
+    }
 
-	[Tooltip("Keys to move downward.")]
-	public List<KeyCode> moveDownwardKeys = new List<KeyCode> { KeyCode.F };
+    void Awake()
+    {
+        webVRManager = WebVRManager.Instance;
+    }
 
-	private WebVRManager webVRManager;
-	private WebVRDisplayCapabilities capabilities;
+    void Start()
+    {
+        WebVRManager.OnVRChange += onVRChange;
+        WebVRManager.OnVRCapabilitiesUpdate += onVRCapabilitiesUpdate;
+        originalRotation = transform.localRotation;
+    }
 
-	bool inDesktopLike {
-		get {
-			return capabilities.hasExternalDisplay;
-		}
-	}
+    private void onVRChange()
+    {
+        if (webVRManager.vrState == WebVRState.ENABLED)
+        {
+            DisableEverything();
+        }
+        else
+        {
+            EnableAccordingToPlatform();
+        }
+    }
 
-	Vector3 mouseMovement = Vector3.zero;
+    private void onVRCapabilitiesUpdate(WebVRDisplayCapabilities vrCapabilities)
+    {
+        capabilities = vrCapabilities;
+        EnableAccordingToPlatform();
+    }
 
-	Vector3 lastMousePosition;
+    void Update() {
+        if (translationEnabled)
+        {
+            float x = Input.GetAxis("Horizontal") * Time.deltaTime * straffeSpeed;
+            float z = Input.GetAxis("Vertical") * Time.deltaTime * straffeSpeed;
 
-	void Awake()
-	{
-		webVRManager = WebVRManager.Instance;
-	}
+            transform.Translate(x, 0, z);
+        }
 
-	void Start()
-	{
-		WebVRManager.OnVRChange += onVRChange;
-		WebVRManager.OnVRCapabilitiesUpdate += onVRCapabilitiesUpdate;
-	}
+        if (rotationEnabled && Input.GetMouseButton(0)) 
+        {
+        
+            rotationX += Input.GetAxis ("Mouse X") * mouseSensitivity;
+            rotationY += Input.GetAxis ("Mouse Y") * mouseSensitivity;
 
-	private void onVRChange()
-	{
-		if (webVRManager.vrState == WebVRState.ENABLED)
-		{
-			DisableEverything();
-		}
-		else
-		{
-			EnableAccordingToPlatform();
-		}
-	}
+            rotationX = ClampAngle (rotationX, minimumX, maximumX);
+            rotationY = ClampAngle (rotationY, minimumY, maximumY);
 
-	private void onVRCapabilitiesUpdate(WebVRDisplayCapabilities vrCapabilities)
-	{
-		capabilities = vrCapabilities;
-		EnableAccordingToPlatform();
-	}
+            Quaternion xQuaternion = Quaternion.AngleAxis (rotationX, Vector3.up);
+            Quaternion yQuaternion = Quaternion.AngleAxis (rotationY, Vector3.left);
 
-	void Update() {
-		if (translationEnabled) {
-			Translate();
-		}
-		if (rotationEnabled) {
-			Rotate();
-		}
-	}
+            transform.localRotation = originalRotation * xQuaternion * yQuaternion;
+        }
+    }
 
-	void DisableEverything() {
-		translationEnabled = false;
-		rotationEnabled = false;
-	}
+    void DisableEverything()
+    {
+        translationEnabled = false;
+        rotationEnabled = false;
+    }
 
-	/// Enables rotation and translation control for desktop environments.
-	/// For mobile environments, it enables rotation or translation according to
-	/// the device capabilities.
-	void EnableAccordingToPlatform() {
-		rotationEnabled = inDesktopLike || !capabilities.hasOrientation;
-		translationEnabled = inDesktopLike || !capabilities.hasPosition;
-	}
+    /// Enables rotation and translation control for desktop environments.
+    /// For mobile environments, it enables rotation or translation according to
+    /// the device capabilities.
+    void EnableAccordingToPlatform()
+    {
+        rotationEnabled = inDesktopLike || !capabilities.hasOrientation;
+        translationEnabled = inDesktopLike || !capabilities.hasPosition;
+    }
 
-	void Translate() {
-		transform.Translate(
-			SideMovement(),
-			ElevationMovement(),
-			ForwardMovement()
-		);
-	}
-
-	void Rotate() {
-		RegisterMouseMovement();
-		transform.Rotate(Vector3.up, YawMovement(), Space.World);
-		transform.Rotate(Vector3.right, -PitchMovement(), Space.Self);
-	}
-
-	float SideMovement() {
-		return TranslationPerFrame(DirectionFromKeys(
-			strideRightKeys,
-			strideLeftKeys
-		));
-	}
-
-	float ElevationMovement() {
-		return TranslationPerFrame(DirectionFromKeys(
-			moveUpwardKeys,
-			moveDownwardKeys
-		));
-	}
-
-	float ForwardMovement() {
-		return TranslationPerFrame(DirectionFromKeys(
-			moveForwardKeys,
-			moveBackwardKeys
-		));
-	}
-
-	float TranslationPerFrame(float direction) {
-		return direction * translationSpeed * Time.deltaTime;
-	}
-
-	float DirectionFromKeys(List<KeyCode> positive, List<KeyCode> negative) {
-		if (AnyKeyIsPressed(positive)) {
-			return 1;
-		}
-		if (AnyKeyIsPressed(negative)) {
-			return -1;
-		}
-		return 0;
-	}
-
-	bool AnyKeyIsPressed(List<KeyCode> keys) {
-		return keys.FindAll(k => Input.GetKey(k)).Count > 0;
-	}
-
-	void RegisterMouseMovement() {
-		bool mouseStoppedDragging = Input.GetMouseButtonUp(0);
-		bool mouseIsDragging = Input.GetMouseButton(0);
-
-		if (mouseStoppedDragging) {
-			mouseMovement.Set(0, 0, 0);
-		}
-		else if (mouseIsDragging) {
-			mouseMovement = Input.mousePosition - lastMousePosition;
-		}
-
-		lastMousePosition = Input.mousePosition;
-	}
-
-	float YawMovement() {
-		return RotationPerFrame(
-			DirectionFromMovement(mouseMovement.x, rotationDeadDistance));
-	}
-
-	float PitchMovement() {
-		return RotationPerFrame(
-			DirectionFromMovement(mouseMovement.y, rotationDeadDistance));
-	}
-
-	float RotationPerFrame(float direction) {
-		return direction * rotationSpeed * Time.deltaTime;
-	}
-
-	float DirectionFromMovement(float number, float threshold=0.001f) {
-		if (number > threshold) {
-			return 1;
-		}
-		if (number < -threshold) {
-			return -1;
-		}
-		return 0;
-	}
+    public static float ClampAngle (float angle, float min, float max)
+    {
+        if (angle < -360f)
+            angle += 360f;
+        if (angle > 360f)
+            angle -= 360f;
+        return Mathf.Clamp (angle, min, max);
+    }
 }
