19826ebbe568d6068e09f1326c45d9eae992f740
Casey Yee <caseyyee.ca@gmail.com>
2018-06-20 17:12:25 -0700

    Scriptable object for input mapping

---
+++ Assets/WebVR/Scripts/WebVRControllerInputMap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "WebVRControllerInputMap")]
+public class WebVRControllerInputMap : ScriptableObject {
+	[Header("WebVR Controller Input Map")]
+	public List<WebVRControllerInput> inputs;
+}
+
+[System.Serializable]
+public class WebVRControllerInput {
+	[Tooltip("A meaningful name describing the gesture performed on the controller.")]
+	public string actionName;
+	[Tooltip("Button ID from Web Gamepad API.")]
+	public int gamepadButtonId;
+	[Tooltip("Button name defined in Unity Input Manager.")]
+	public string unityInputName;
+}
