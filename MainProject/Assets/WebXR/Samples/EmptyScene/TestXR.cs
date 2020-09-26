17f6b3d0fa77b78d97ca57d6fd1e0be5e2b30d2a
De-Panther <depanther@gmail.com>
2020-09-27 01:24:20 +0300

    Test XR SDK, without the XR SDK files

---
+++ MainProject/Assets/WebXR/Samples/EmptyScene/TestXR.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class TestXR : MonoBehaviour
+{
+  // Start is called before the first frame update
+  void Start()
+  {
+    var xrDisplaySubsystems = new List<XRDisplaySubsystem>();
+    SubsystemManager.GetInstances<XRDisplaySubsystem>(xrDisplaySubsystems);
+    Debug.LogError("Getting xrDisplaySubsystems " + xrDisplaySubsystems.Count);
+    foreach (var xrDisplay in xrDisplaySubsystems)
+    {
+      Debug.LogError($"xrDisplay running {xrDisplay.running}");
+    }
+  }
+}
--- Packages/webxr/Runtime/XRPlugin/WebXRLoader.cs
+++ Packages/webxr/Runtime/XRPlugin/WebXRLoader.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
+using UnityEngine.XR;
 using UnityEngine.XR.Management;
 
 namespace WebXR
@@ -7,12 +9,27 @@ namespace WebXR
     public class WebXRLoader : XRLoaderHelper
     {
         static readonly List<WebXRSubsystemDescriptor> sampleSubsystemDescriptors = new List<WebXRSubsystemDescriptor>();
+        static readonly List<XRDisplaySubsystemDescriptor> displaySubsystemDescriptors = new List<XRDisplaySubsystemDescriptor>();
+        static readonly List<XRInputSubsystemDescriptor> inputSubsystemDescriptors = new List<XRInputSubsystemDescriptor>();
         public WebXRSubsystem WebXRSubsystem => GetLoadedSubsystem<WebXRSubsystem>();
-        
+        public XRDisplaySubsystem XRDisplaySubsystem => GetLoadedSubsystem<XRDisplaySubsystem>();
+        public XRInputSubsystem XRInputSubsystem => GetLoadedSubsystem<XRInputSubsystem>();
+
+#if !UNITY_EDITOR && UNITY_WEBGL
+        [DllImport("__Internal")]
+        private static extern void RegisterWebXRPlugin();
+#endif
+
         public override bool Initialize()
         {
             Debug.Log("Initialize " + nameof(WebXRLoader));
+            #if !UNITY_EDITOR && UNITY_WEBGL
+            RegisterWebXRPlugin();
+            Debug.Log("Called RegisterWebXRPlugin");
+            #endif
             CreateSubsystem<WebXRSubsystemDescriptor, WebXRSubsystem>(sampleSubsystemDescriptors, typeof(WebXRSubsystem).FullName);
+            CreateSubsystem<XRDisplaySubsystemDescriptor, XRDisplaySubsystem>(displaySubsystemDescriptors, "WebXR VR Display");
+            CreateSubsystem<XRInputSubsystemDescriptor, XRInputSubsystem>(inputSubsystemDescriptors, "WebXR Inputs");
             return WebXRSubsystem != null;
         }
         
@@ -20,18 +37,27 @@ namespace WebXR
         public override bool Start()
         {
             WebXRSubsystem.Start();
+            Debug.Log("Start XRDisplaySubsystem");
+            XRDisplaySubsystem.Start();
+            Debug.Log("Did it work?!? XRDisplaySubsystem");
+            XRInputSubsystem.Start();
             return true;
         }
 
         public override bool Stop()
         {
             WebXRSubsystem.Stop();
+            XRDisplaySubsystem.Stop();
+            XRInputSubsystem.Stop();
             return base.Stop();
         }
 
-        private void OnDestroy()
+        public override bool Deinitialize()
         {
             WebXRSubsystem.Destroy();
+            XRDisplaySubsystem.Destroy();
+            XRInputSubsystem.Destroy();
+            return base.Deinitialize();
         }
     }
 }
