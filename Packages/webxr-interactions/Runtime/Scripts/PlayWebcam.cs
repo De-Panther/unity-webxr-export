1fda06e6c8c6b0b57161570a72df21cab26e1cf1
De-Panther <depanther@gmail.com>
2022-10-18 00:12:41 +0300

    Add an option to set webcam threshold colors from UI

--- Packages/webxr-interactions/Runtime/Scripts/PlayWebcam.cs
+++ Packages/webxr-interactions/Runtime/Scripts/PlayWebcam.cs
@@ -3,16 +3,52 @@ using UnityEngine;
 
 public class PlayWebcam : MonoBehaviour
 {
+  [SerializeField]
+  private string thresholdMinName = "_ThresholdMin";
+  [SerializeField]
+  private string thresholdMaxName = "_ThresholdMax";
+
   private WebCamTexture webcamTexture;
   private Renderer _renderer;
   private bool started = false;
+
+  private Material material;
+  private bool hasThresholdProperties = false;
+  private int thresholdMinID = 0;
+  private int thresholdMaxID = 0;
+  private Color thresholdMinColor;
+  private Color thresholdMaxColor;
 
   private int defaultWidth = 1280;
   private int defaultHeight = 720;
 
   void Awake()
   {
-    _renderer = GetComponent<Renderer>();
+    TrySetupRenderer();
+  }
+
+  void TrySetupRenderer()
+  {
+    if (_renderer == null)
+    {
+      _renderer = GetComponent<Renderer>();
+      material = Instantiate(_renderer.sharedMaterial);
+      _renderer.sharedMaterial = material;
+      hasThresholdProperties = false;
+      thresholdMinID = material.shader.FindPropertyIndex(thresholdMinName);
+      if (thresholdMinID > -1)
+      {
+        thresholdMinID = material.shader.GetPropertyNameId(thresholdMinID);
+      }
+      thresholdMaxID = material.shader.FindPropertyIndex(thresholdMaxName);
+      if (thresholdMaxID > -1)
+      {
+        thresholdMaxID = material.shader.GetPropertyNameId(thresholdMaxID);
+      }
+      thresholdMinColor = material.GetColor(thresholdMinID);
+      thresholdMaxColor = material.GetColor(thresholdMaxID);
+      hasThresholdProperties = true;
+    }
   }
 
   void Start()
@@ -38,7 +74,7 @@ public class PlayWebcam : MonoBehaviour
       }
       float ratio = (float)defaultWidth / (float)defaultHeight;
       transform.localScale = new Vector3(ratio, 1f, 1f);
-      _renderer.material.mainTexture = webcamTexture;
+      material.mainTexture = webcamTexture;
     }
     webcamTexture.Play();
     StartCoroutine(SetScale());
@@ -66,5 +102,61 @@ public class PlayWebcam : MonoBehaviour
   {
     StopAllCoroutines();
     webcamTexture.Stop();
+  }
+
+  private void TrySetColor(string value, ref Color color, int propertyID, int colorLetter)
+  {
+    if (hasThresholdProperties && int.TryParse(value, out int intValue))
+    {
+      switch (colorLetter)
+      {
+        case 0:
+          color.r = (float)Mathf.Clamp(intValue, 0, 255) / 255f;
+          break;
+        case 1:
+          color.g = (float)Mathf.Clamp(intValue, 0, 255) / 255f;
+          break;
+        case 2:
+          color.b = (float)Mathf.Clamp(intValue, 0, 255) / 255f;
+          break;
+      }
+      material.SetColor(propertyID, color);
+    }
+  }
+
+  public void TrySetMinR(string value)
+  {
+    TrySetupRenderer();
+    TrySetColor(value, ref thresholdMinColor, thresholdMinID, 0);
+  }
+
+  public void TrySetMinG(string value)
+  {
+    TrySetupRenderer();
+    TrySetColor(value, ref thresholdMinColor, thresholdMinID, 1);
+  }
+
+  public void TrySetMinB(string value)
+  {
+    TrySetupRenderer();
+    TrySetColor(value, ref thresholdMinColor, thresholdMinID, 2);
+  }
+
+  public void TrySetMaxR(string value)
+  {
+    TrySetupRenderer();
+    TrySetColor(value, ref thresholdMaxColor, thresholdMaxID, 0);
+  }
+
+  public void TrySetMaxG(string value)
+  {
+    TrySetupRenderer();
+    TrySetColor(value, ref thresholdMaxColor, thresholdMaxID, 1);
+  }
+
+  public void TrySetMaxB(string value)
+  {
+    TrySetupRenderer();
+    TrySetColor(value, ref thresholdMaxColor, thresholdMaxID, 2);
   }
 }
