6d6799be4566f3d1ce99ec294ecfc77843b3f543
De-Panther <depanther@gmail.com>
2024-01-16 20:17:36 +0200

    Added error message when there's no Universal Render Pipeline package in project

--- Packages/webxr/Editor/WebXRBuildProcessor.cs
+++ Packages/webxr/Editor/WebXRBuildProcessor.cs
@@ -8,46 +8,46 @@ namespace WebXR
 {
   public class WebXRBuildProcessor : IPreprocessBuildWithReport, IPostprocessBuildWithReport
   {
-        /// <summary>Override of <see cref="IPreprocessBuildWithReport"/> and <see cref="IPostprocessBuildWithReport"/></summary>
-        public int callbackOrder
+    /// <summary>Override of <see cref="IPreprocessBuildWithReport"/> and <see cref="IPostprocessBuildWithReport"/></summary>
+    public int callbackOrder
+    {
+      get { return 0; }
+    }
+
+    void CleanOldSettings()
+    {
+      UnityEngine.Object[] preloadedAssets = PlayerSettings.GetPreloadedAssets();
+      if (preloadedAssets == null)
+        return;
+
+      var oldSettings = from s in preloadedAssets
+                        where s != null && s.GetType() == typeof(WebXRSettings)
+                        select s;
+
+      if (oldSettings != null && oldSettings.Any())
+      {
+        var assets = preloadedAssets.ToList();
+        foreach (var s in oldSettings)
         {
-            get { return 0;  }
+          assets.Remove(s);
         }
 
-        void CleanOldSettings()
-        {
-            UnityEngine.Object[] preloadedAssets = PlayerSettings.GetPreloadedAssets();
-            if (preloadedAssets == null)
-                return;
+        PlayerSettings.SetPreloadedAssets(assets.ToArray());
+      }
+    }
 
-            var oldSettings = from s in preloadedAssets
-                where s != null && s.GetType() == typeof(WebXRSettings)
-                select s;
+    /// <summary>Override of <see cref="IPreprocessBuildWithReport"/></summary>
+    /// <param name="report">Build report.</param>
+    public void OnPreprocessBuild(BuildReport report)
+    {
+      // Always remember to cleanup preloaded assets after build to make sure we don't
+      // dirty later builds with assets that may not be needed or are out of date.
+      CleanOldSettings();
 
-            if (oldSettings != null && oldSettings.Any())
-            {
-                var assets = preloadedAssets.ToList();
-                foreach (var s in oldSettings)
-                {
-                    assets.Remove(s);
-                }
-
-                PlayerSettings.SetPreloadedAssets(assets.ToArray());
-            }
-        }
-
-        /// <summary>Override of <see cref="IPreprocessBuildWithReport"/></summary>
-        /// <param name="report">Build report.</param>
-        public void OnPreprocessBuild(BuildReport report)
-        {
-            // Always remember to cleanup preloaded assets after build to make sure we don't
-            // dirty later builds with assets that may not be needed or are out of date.
-            CleanOldSettings();
-
-            if (report.summary.platform != BuildTarget.WebGL)
-            {
-              return;
-            }
+      if (report.summary.platform != BuildTarget.WebGL)
+      {
+        return;
+      }
 
 #if UNITY_2020_3 || UNITY_2021_1
             if (!PlayerSettings.WebGL.emscriptenArgs.Contains("-std="))
@@ -56,28 +56,36 @@ namespace WebXR
             }
 #endif
 
-            WebXRSettings settings = null;
-            EditorBuildSettings.TryGetConfigObject<WebXRSettings>("WebXR.Settings", out settings);
-            if (settings == null)
-                return;
+      WebXRSettings settings = WebXRSettings.GetSettings();
+      if (settings == null)
+        return;
 
-            UnityEngine.Object[] preloadedAssets = PlayerSettings.GetPreloadedAssets();
+      UnityEngine.Object[] preloadedAssets = PlayerSettings.GetPreloadedAssets();
 
-            if (!preloadedAssets.Contains(settings))
-            {
-                var assets = preloadedAssets.ToList();
-                assets.Add(settings);
-                PlayerSettings.SetPreloadedAssets(assets.ToArray());
-            }
-        }
+      if (!preloadedAssets.Contains(settings))
+      {
+        var assets = preloadedAssets.ToList();
+        assets.Add(settings);
+        PlayerSettings.SetPreloadedAssets(assets.ToArray());
+      }
+    }
 
-        /// <summary>Override of <see cref="IPostprocessBuildWithReport"/></summary>
-        /// <param name="report">Build report.</param>
-        public void OnPostprocessBuild(BuildReport report)
-        {
-            // Always remember to cleanup preloaded assets after build to make sure we don't
-            // dirty later builds with assets that may not be needed or are out of date.
-            CleanOldSettings();
-        }
+    /// <summary>Override of <see cref="IPostprocessBuildWithReport"/></summary>
+    /// <param name="report">Build report.</param>
+    public void OnPostprocessBuild(BuildReport report)
+    {
+      // Always remember to cleanup preloaded assets after build to make sure we don't
+      // dirty later builds with assets that may not be needed or are out of date.
+      CleanOldSettings();
+    }
+
+#if !HAS_URP
+    [InitializeOnLoadMethod]
+    private static void OnPostprocessAssets()
+    {
+      UnityEngine.Debug.LogError(@"WebXR Export requires Universal Render Pipeline,
+using Built-in Render Pipeline might cause issues.");
+    }
+#endif
   }
 }
